c6e83ca74504717c055ecc4ed1b9938a18d2c865
stanlay <69060811+eostanlay@users.noreply.github.com>
2022-07-04 00:49:41 -0300

    Add files via upload
    made a sky background that changes with the theme

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const changer = document.getElementById('changer')
 
 changer.addEventListener('change', () => {
-  document.body.classList.toggle('dark')
+  document.body.classList.toggle('light')
+  document.getElementById('sky').classList.toggle('light')
+  //if you want to add a div just set div id and add it here.
 })
